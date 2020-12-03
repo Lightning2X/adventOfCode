6d5542feb5a355ee7ada82bc75a1de8ff8306825
Tim Schagen <schagen34@gmail.com>
2020-12-04 00:23:36 +0100

    Solve 3-2

--- src/Day3.cs
+++ src/Day3.cs
@@ -12,15 +12,34 @@ namespace Lightning2x.AdventOfCode2020
             string[] grid = Utils.ReadFile(path).ToArray();
             // Amount of characters before we wrap around the list.
             int wrap = grid[0].Length;
+            List<(int, int)> slopes = new List<(int, int)>() {
+                (1, 1),
+                (3, 1),
+                (5, 1),
+                (7, 1),
+                (1, 2)
+                };
+            int part2Result = 1;
+            foreach ((int, int) s in slopes)
+            {
+                part2Result *= TreeSlopes(grid, wrap, s.Item1, s.Item2);
+            }
+            Console.WriteLine("Total Amount of trees for Part 1 is: " + TreeSlopes(grid, wrap, 3, 1));
+            Console.WriteLine($"Total Amount of trees for Part 2 is: {part2Result}");
+        }
+
+        public int TreeSlopes(string[] grid, int wrap, int dx, int dy)
+        {
             int treeAmount = 0;
             int xCoord = 0;
-            for (int y = 0; y < grid.Length; y++)
+            for (int y = 0; y < grid.Length; y += dy)
             {
                 if (grid[y][xCoord % wrap] == '#')
                     treeAmount++;
-                xCoord += 3;
+                xCoord += dx;
             }
-            Console.WriteLine($"Total Amount of trees for Part 1 is: {treeAmount}");
+
+            return treeAmount;
         }
     }
 }
