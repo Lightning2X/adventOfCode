1c3bfcc6a50d6a91582c7f12d6fcf64f0275beec
Tim Schagen <schagen34@gmail.com>
2020-12-04 15:13:19 +0100

    Solve 4-2

--- src/Day4.cs
+++ src/Day4.cs
@@ -28,11 +28,17 @@ namespace Lightning2x.AdventOfCode2020
                 }
             }
             List<Passport> passports = Utils.TypeParser(passportStrings, Passport.Parse);
-            int validPassports = 0;
+            int passportsValidKeys = 0;
+            int passportsValid = 0;
             foreach (Passport p in passports)
-                if (p.IsValid)
-                    validPassports++;
-            Console.WriteLine($"The amount of valid passports for part 1 is {validPassports}");
+            {
+                if (p.HasValidKeys)
+                    passportsValidKeys++;
+                if (p.IsValidPassport)
+                    passportsValid++;
+            }
+            Console.WriteLine($"The amount of valid passports for part 1 is {passportsValidKeys}");
+            Console.WriteLine($"The amount of valid passports for part 2 is {passportsValid}");
         }
 
         private class Passport
@@ -44,8 +50,57 @@ namespace Lightning2x.AdventOfCode2020
                 passportFields = new Dictionary<string, string>();
             }
 
-            public bool IsValid => expectedFields.All(passportFields.ContainsKey);
-            
+            public bool HasValidKeys => expectedFields.All(passportFields.ContainsKey);
+
+            public bool IsValidPassport
+            {
+                get
+                {
+                    if (!HasValidKeys)
+                        return false;
+                    else return BirthYearValid && IssuerYearValid && ExpirationYearValid && HeightValid && HairColorValid && EyeColorValid && PassportIdValid;
+                }
+            }
+
+            private bool KeyInRange(string key, int min, int max)
+            {
+                int val;
+                if (int.TryParse(passportFields[key], out val))
+                    return val >= min && val <= max;
+                else
+                    return false;
+            }
+            private bool BirthYearValid => KeyInRange("byr", 1920, 2002);
+            private bool IssuerYearValid => KeyInRange("iyr", 2010, 2020);
+            private bool ExpirationYearValid => KeyInRange("eyr", 2020, 2030);
+            private bool HeightValid
+            {
+                get
+                {
+                    string heightIndicator = passportFields["hgt"].Substring(passportFields["hgt"].Length - 2);
+                    string heightString = passportFields["hgt"].Substring(0, passportFields["hgt"].Length - 2);
+                    int height;
+                    if (!int.TryParse(heightString, out height))
+                        return false;
+                    if (heightIndicator == "in")
+                        return height >= 59 && height <= 76;
+                    else if (heightIndicator == "cm")
+                        return height >= 150 && height <= 193;
+                    else
+                        return false;
+                }
+            }
+            private bool HairColorValid => passportFields["hcl"][0] == '#' && passportFields["hcl"].Length == 7 && int.TryParse(passportFields["hcl"].Substring(1), System.Globalization.NumberStyles.HexNumber, null, out _);
+            private bool EyeColorValid
+            {
+                get
+                {
+                    string[] validEyeColors = new string[] { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+                    return validEyeColors.Any(c => c == passportFields["ecl"]);
+                }
+            }
+
+            private bool PassportIdValid => passportFields["pid"].Length == 9 && int.TryParse(passportFields["pid"], out _);
             public static Passport Parse(string input)
             {
                 //if (String.IsNullOrWhiteSpace(input)) throw new ArgumentException(input);
